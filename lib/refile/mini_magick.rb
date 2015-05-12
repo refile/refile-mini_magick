fa15457613f42be00bf304b22a20e87294ece4a7
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-05-12 14:10:16 +0200

    Rewrite fill method to *far* simpler implementation
    Yay tests!

--- lib/refile/mini_magick.rb
+++ lib/refile/mini_magick.rb
@@ -63,27 +63,10 @@ module Refile
     # @return [void]
     # @see http://www.imagemagick.org/script/command-line-options.php#gravity
     def fill(img, width, height, gravity = "Center")
-      # FIXME: test and rewrite to simpler implementation!
-      width = width.to_i
-      height = height.to_i
-      cols, rows = img[:dimensions]
       img.combine_options do |cmd|
-        if width != cols || height != rows
-          scale_x = width / cols.to_f
-          scale_y = height / rows.to_f
-          if scale_x >= scale_y
-            cols = (scale_x * (cols + 0.5)).round
-            rows = (scale_x * (rows + 0.5)).round
-            cmd.resize "#{cols}"
-          else
-            cols = (scale_y * (cols + 0.5)).round
-            rows = (scale_y * (rows + 0.5)).round
-            cmd.resize "x#{rows}"
-          end
-        end
+        cmd.resize "#{width}x#{height}^"
         cmd.gravity gravity
-        cmd.background "rgba(255,255,255,0.0)"
-        cmd.extent "#{width}x#{height}" if cols != width || rows != height
+        cmd.extent "#{width}x#{height}"
       end
     end
 
